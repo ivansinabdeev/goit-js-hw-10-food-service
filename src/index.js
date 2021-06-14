a7112dd5b762d31c24b366855dc3c96700ea8241
ivansinabdeev <ivansinabdeev@gmail.com>
2021-06-14 22:20:07 +0200

    adds js

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import menuItems from "../src/menu.json";
+import menuTemplate from "../src/templates//menu.hbs";
+const menuGallery = document.querySelector(".menu");
+const markup = menuTemplate(menuItems);
+menuGallery.insertAdjacentHTML("afterbegin", markup);
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
+};
+const body = document.querySelector("body");
+const defaultTheme = body.classList.add("light-theme");
+const button = document.querySelector("#theme-switch-toggle");
+button.onchange = function () {
+  if (button.checked === true) {
+    body.classList.add("dark-theme");
+    body.classList.remove("light-theme");
+    localStorage.setItem("theme-color", Theme.DARK);
+  } else {
+    body.classList.remove("dark-theme");
+    body.classList.add("light-theme");
+    localStorage.setItem("theme-color", Theme.LIGHT);
+  }
+};
+const savedTheme = localStorage.getItem("theme-color");
+if (savedTheme === Theme.DARK) {
+  button.checked = true;
+  body.classList.add("dark-theme");
+}
+if (savedTheme === Theme.LIGHT) {
+  button.checked = false;
+  body.classList.add("light-theme");
+}
